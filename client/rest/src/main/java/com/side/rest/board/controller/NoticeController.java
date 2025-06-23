df1f018c6e04502b48dc7c6caf8bbaf723155dcd
taehoon-200 <dxr200@gmail.com>
2025-06-23 23:58:39 +0900

    MP-3 feat(notice): 공지사항 등록기능 구현

--- client/rest/src/main/java/com/side/rest/board/controller/NoticeController.java
+++ client/rest/src/main/java/com/side/rest/board/controller/NoticeController.java
@@ -2,11 +2,13 @@ package com.side.rest.board.controller;
 
 import static com.side.rest.mapper.NoticeMapper.*;
 
+import com.side.domain.model.Notice;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,28 +25,41 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class NoticeController {
 
-	private final NoticeUseCase noticeUseCase;
+    private final NoticeUseCase noticeUseCase;
 
 
 
-	// @PostMapping
-	// public ResponseEntity<Void> create(@RequestBody(required = false) NoticeRequestDto noticeRequestDto) {
-	//
-	// 	noticeUseCase.create(Notice.builder()
-	// 										  .article(Article.builder()
-	// 														  .title("Test Title")
-	// 														  .contents("Test Contents")
-	// 														  .build())
-	// 										  .build());
-	//
-	// 	// noticeUseCase.create(NoticeMapper.toDomain(noticeRequestDto));
-	//
-	// 	return ResponseEntity.ok().build();
-	// }
+    // @PostMapping
+    // public ResponseEntity<Void> create(@RequestBody(required = false) NoticeRequestDto noticeRequestDto) {
+    //
+    // 	noticeUseCase.create(Notice.builder()
+    // 										  .article(Article.builder()
+    // 														  .title("Test Title")
+    // 														  .contents("Test Contents")
+    // 														  .build())
+    // 										  .build());
+    //
+    // 	// noticeUseCase.create(NoticeMapper.toDomain(noticeRequestDto));
+    //
+    // 	return ResponseEntity.ok().build();
+    // }
 
-	@GetMapping
-	public ResponseEntity<NoticeResponseDto> get(NoticeRequestDto noticeRequestDto) {
+    @GetMapping
+    public ResponseEntity<NoticeResponseDto> get(NoticeRequestDto noticeRequestDto) {
 
-		return ResponseEntity.ok(NoticeMapper.toResponse(noticeUseCase.find(NoticeMapper.toDomain(noticeRequestDto))));
-	}
+        return ResponseEntity.ok(NoticeMapper.toResponse(noticeUseCase.find(NoticeMapper.toDomain(noticeRequestDto))));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createNotice(@RequestBody NoticeRequestDto noticeRequestDto) {
+
+        Notice notice = NoticeMapper.toDomain(noticeRequestDto);
+
+        noticeUseCase.create(notice);
+
+        return ResponseEntity.ok("게시글을 성공적으로 작성했습니다");
+    }
+
+
 }
